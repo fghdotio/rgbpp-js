4b1736f3f54ff8a3aa72069c92662f7b167f2d43
fgh_ssh <fghdotio@gmail.com>
2025-01-18 03:54:48 -0600

    why does case matter here

--- packages/core/src/constants/index.ts
+++ packages/core/src/constants/index.ts
@@ -5,6 +5,6 @@ export const XUDT_LIKE_ISSUANCE_OUTPUT_INDEX = 1;
 
 export const DEFAULT_CONFIRMATIONS = 6;
 
-export const RGBPP_CKB_WITNESS_PLACEHOLDER = "0xFF";
+export const RGBPP_CKB_WITNESS_PLACEHOLDER = "0xff";
 
 export const RGBPP_MAX_CELL_NUM = 255;
